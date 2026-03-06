a241d736037c6cd2f09ccd45c0d8c2d0db4599d5
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-03-06 21:46:40 +0545

    Remove user login and signup routes from App component.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -20,11 +20,7 @@ const App = () => {
         <Route path="/aboutus" element={<Aboutus />} />
         <Route path="/contact" element={<Contact />} />
 
-        {/* Logins User*/}
-        <Route path="/login" element={<Userlogin />} />
-        <Route path="/signup" element={<UserSignup />} /> 
-
-
+    
       </Routes>
       <WhatsAppChat />
     </BrowserRouter>
