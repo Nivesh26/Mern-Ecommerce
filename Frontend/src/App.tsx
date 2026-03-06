ae5e3d1e5eca5a2b121f666a49990d8c07ae1081
Nivesh Shrestha <nivesh.shrestha.94@gmail.com>
2026-03-06 21:47:09 +0545

    Add admin login and signup routes to App component.

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -24,6 +24,10 @@ const App = () => {
         <Route path="/login" element={<Userlogin />} />
         <Route path="/signup" element={<UserSignup />} /> 
 
+        {/* Logins Admin*/}
+        <Route path="/adminlogin" element={<Adminlogin />} />
+        <Route path="/adminsignup" element={<AdminSignup />} />
+
 
       </Routes>
       <WhatsAppChat />
